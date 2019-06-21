f55df40295e21b04ae7cddb5b1d6d838803797cd
Fiki Maulana <fiki@ecampuz.com>
2019-06-21 14:59:03 +0700

    Reverse string before encript

--- EnkripsiClass.php
+++ EnkripsiClass.php
@@ -10,11 +10,11 @@
 				$str .= $string{$i}.$random{mt_rand(0,strlen($random)-1)};
 			endfor;
 			
-			return base64_encode($str);
+			return base64_encode(strrev($str));
 		}
 
 		function Dec($string){
-			$string_dec = base64_decode($string);
+			$string_dec = strrev(base64_decode($string));
 			$str = "";
 			for($i=0;$i<strlen($string_dec);$i+=2):
 				$str .= $string_dec{$i};
